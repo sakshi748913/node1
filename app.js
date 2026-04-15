3292180d94642f23699ec9aca32efc07dd78ac43
sakshi748913 <patwasakshi46@gmail.com>
2026-04-16 00:55:57 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+// console.log(global);
+// global.console.log("sakshi")
+// globalThis.console.log("patwa")
+// globalThis.console.log(module)
+// console.log(globalThis.module)
+// console.log(window.document)
+
+
+// commonjs
+
+
+// react - components
+// naode - module
+
+// node modules
+
+// commonjs
+const add = require("./math");
+
+console.log(add(5,10));
---
+++ math.js
@@ -0,0 +1,12 @@
+// encpsulation
+
+// common js module system(model export and reaquire)
+
+// code moduler maintable and easer to debug.
+// import to export 
+
+const add = (a,b) => {
+    return a + b ;
+
+}
+module.exports = add;
